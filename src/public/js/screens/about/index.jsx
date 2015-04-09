47adaa13a1b1455bbe07a8318e2312775294bec7
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-04-09 14:13:36 +0200

    Implemented about screen

--- src/public/js/screens/about/index.jsx
+++ src/public/js/screens/about/index.jsx
@@ -1,16 +1,30 @@
 var React = require('react');
 var Reflux = require('reflux');
+var cx = require('react/lib/cx');
 
+var Router = require('react-router');
+var Link = Router.Link;
 
 module.exports = React.createClass({
+
 	render: function () {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="small-12">
-						About page
-					</div>
-				</div>
+			<div className="container center-absolute">
+				<h2>Example body text</h2>
+
+				<p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis
+					dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
+
+				<p>
+					<small>This line of text is meant to be treated as fine print.</small>
+				</p>
+				<p>The following snippet of text is <strong>rendered as bold text</strong>.</p>
+
+				<p>The following snippet of text is <em>rendered as italicized text</em>.</p>
+
+				<p>An abbreviation of the word attribute is <abbr title="attribute">attr</abbr>.</p>
+
+				<Link className="btn  btn-success" to="game"> Back to game </Link>
 			</div>
 		);
 	}
