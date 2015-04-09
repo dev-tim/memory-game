a06eef2bbd669c0018b792cacc5ba76e6ba29f22
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-04-09 14:12:35 +0200

    Improved photos service

--- src/public/js/services/photos-service.js
+++ src/public/js/services/photos-service.js
@@ -5,6 +5,13 @@ module.exports = {
 		return fetch('/photos')
 			.then(function asJson (resp) {
 				return resp.json();
+			}).then(function transformPhotos (data) {
+				return data.photos.items.map(function (photo) {
+					return {
+						id: photo.id,
+						url: photo.photoUrl
+					}
+				})
 			})
 	}
 };
